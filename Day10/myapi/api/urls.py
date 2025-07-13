830cb524b3a4081bc544387b88f69230ad3442b4
Muhammad Maroof <maroof96965@gmail.com>
2025-07-13 10:03:10 +0500

    day10

---
+++ Day10/myapi/api/urls.py
@@ -0,0 +1,8 @@
+# urls.py in the app
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('products/', views.product_list_create, name='product-list'),
+    path('products/<int:pk>/', views.product_detail, name='product-detail'),
+]
